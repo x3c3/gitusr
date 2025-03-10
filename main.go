49b876e592b91b7745dc4c00a84f4c953e523eb0
reda <business.reda@pm.me>
2025-03-10 21:39:49 +0000

    chore: update main.go

--- main.go
+++ main.go
@@ -1,81 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"path/filepath"
-	"regexp"
-
-	"github.com/fatih/color"
-	"github.com/surbytes/gitusr/models"
 	"github.com/surbytes/gitusr/utils"
-	"gopkg.in/ini.v1"
 )
 
 func main() {
 
-	if len(os.Args) > 1 {
-		models.SetUsr(os.Args[1])
-	}
-
-	home, err := os.UserHomeDir()
-	utils.CheckErr(err)
-
-	gitconfig := filepath.Join(home, ".gitconfig")
-
-	file, err := os.Open(gitconfig)
-	utils.CheckErr(err)
-	defer file.Close()
-
-	//re := regexp.MustCompile(`\[users\.(.*?)\]`)
-	re := regexp.MustCompile(`\[users\s+"(.*?)"\]`)
-
-	var usersKeys []string
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		match := re.FindStringSubmatch(line)
-		if len(match) > 1 {
-			usersKeys = append(usersKeys, match[1])
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("error reading file")
-		return
-	}
-
-	//fmt.Println(usersKeys)
-
-	initdata, err := ini.Load(gitconfig)
-	utils.CheckErr(err)
-	var users []models.User
-	for _, v := range usersKeys {
-		u := "users \"" + v + "\""
-		//fmt.Println(u)
-		section := initdata.Section(u)
-		name, err := section.GetKey("name")
-		utils.CheckErr(err)
-		email, err := section.GetKey("email")
-		utils.CheckErr(err)
-		user := models.User{
-			Name:  name.String(),
-			Email: email.String(),
-		}
-		users = append(users, user)
-	}
-
-	users = append(users, models.GetCurrentUsr())
-
-	for _, usr := range users {
-		//utils.PrintInfo("%s <%s>", usr.Name, usr.Email)
-		if usr.Name == models.GetCurrentUsr().Name && usr.Email == models.GetCurrentUsr().Email {
-			color.Yellow("%s <%s> *", usr.Name, usr.Email)
-		} else {
-			fmt.Printf("%s <%s>\n", usr.Name, usr.Email)
-		}
-	}
+	utils.RenderUsers()
 
 }
