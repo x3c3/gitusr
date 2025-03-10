0ff580cbfc9e951b9498c22962393a957ba78c1e
yo <yo@yod>
2025-03-10 14:30:29 +0000

    feat: add utils

---
+++ utils/utils.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+// print a message
+func PrintInfo(format string, args ...interface{}) {
+	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
+
+// handle error
+func CheckErr(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
+}
